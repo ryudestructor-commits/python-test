9d0d569229a3be11ce3bff1e633d97413ab42d3d
ryudestructor-commits <ryudestructor@gmail.com>
2026-07-24 19:05:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hola")
+import http.server
+import socketserver
+import os
+
+PORT = int(os.environ.get("PORT", 10000))
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Servidor iniciado en el puerto", PORT)
+    httpd.serve_forever()
