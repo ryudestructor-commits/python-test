b4b5b2c4870d84a97aca0d620a2772c3302871d0
ryudestructor-commits <ryudestructor@gmail.com>
2026-07-25 06:05:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import http.server
-import socketserver
 import os
+import telebot
 
-PORT = int(os.environ.get("PORT", 10000))
-Handler = http.server.SimpleHTTPRequestHandler
+# Carga el token de manera segura desde Render
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+bot = telebot.TeleBot(TOKEN)
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Servidor iniciado en el puerto", PORT)
-    httpd.serve_forever()
+@bot.message_handler(commands=['start'])
+def enviar_bienvenida(message):
+    nombre = message.from_user.first_name
+    texto = f"¡Hola, {nombre}! 👋\n\nSoy **Caza Errores Top**, tu bot personal ya funcionando 24/7."
+    bot.reply_to(message, texto, parse_mode="Markdown")
+
+bot.infinity_polling()
